baa1b65117b8a1fd945fb654004137209fd54931
Florent ROBERT <florent.robert@etu.unice.fr>
2021-01-22 19:04:26 +0100

    init unity project

---
+++ KakoophonieUnity/Kakoophonie/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    public void LoadProf(){
+        SceneManager.LoadScene("Professeur");
+    }
+
+    public void LoadEleve(){
+        SceneManager.LoadScene("Eleve");
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+}
