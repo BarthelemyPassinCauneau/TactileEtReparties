20af84a7d53eeaf3563a2d6339b5bec3d6a1f31d
Barthélemy Passin-Cauneau <34715570+BarthelemyPassinCauneau@users.noreply.github.com>
2021-02-19 09:39:15 +0100

    rework waiting room

--- KakoophonieUnity/Kakoophonie/Assets/Scripts/Network/ParticipantsListingMenu.cs
+++ KakoophonieUnity/Kakoophonie/Assets/Scripts/Network/ParticipantsListingMenu.cs
@@ -4,40 +4,46 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using System;
+using TMPro;
 
 public class ParticipantsListingMenu : MonoBehaviourPunCallbacks
 {
     [SerializeField] private ParticipantItem _participantItem = null;
     [SerializeField] private Transform _content = null;
     [SerializeField] private GameObject _startButton = null;
+    [SerializeField] TMP_Text ProfTitle = null;
+    [SerializeField] TMP_Text NbStudentsText = null;
 
-    private Dictionary<int, ParticipantItem> ParticipantsList = new Dictionary<int, ParticipantItem>();
+    private Dictionary<Player, ParticipantItem> ParticipantsList = new Dictionary<Player, ParticipantItem>();
 
     private void Start() {
-        RefreshList();
-        _startButton.SetActive(PhotonNetwork.IsMasterClient);
-    }
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        ParticipantItem participant = Instantiate(_participantItem, _content);
-        ParticipantsList[newPlayer.ActorNumber] = participant;
-        participant.SetParticipantInfo(newPlayer.NickName);
-    }
-
-    public void RefreshList() {
         foreach (Player player in PhotonNetwork.PlayerList)
         {
-            if(!ParticipantsList.ContainsKey(player.ActorNumber)) {
+            if(player.IsMasterClient) {
+                ProfTitle.text += player.NickName;
+            } else {
                 ParticipantItem participant = Instantiate(_participantItem, _content);
-                ParticipantsList[player.ActorNumber] = participant;
+                ParticipantsList[player] = participant;
                 participant.SetParticipantInfo(player.NickName);
             }
         }
+        NbStudentsText.text = "Nb d'élèves : " + (PhotonNetwork.PlayerList.Length-1);
+        _startButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    // FONCTIONS SURCHARGEES FOURNIES PAR PHOTON
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        ParticipantItem participant = Instantiate(_participantItem, _content);
+        ParticipantsList[newPlayer] = participant;
+        participant.SetParticipantInfo(newPlayer.NickName);
+        NbStudentsText.text = "Nb d'élèves : " + (PhotonNetwork.PlayerList.Length-1);
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
-        Destroy(ParticipantsList[otherPlayer.ActorNumber].gameObject);
-        ParticipantsList.Remove(otherPlayer.ActorNumber);
+        Destroy(ParticipantsList[otherPlayer].gameObject);
+        ParticipantsList.Remove(otherPlayer);
+        NbStudentsText.text = "Nb d'élèves : " + (PhotonNetwork.PlayerList.Length-1);
     }
 }
